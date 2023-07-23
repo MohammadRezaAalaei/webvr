4eefe69958b2cf5893df9039dbda8767ba045c60
Mohammadreza <Mohammadrezaaalaei@gmail.com>
2023-07-23 23:00:31 +0330

    new Orbit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
 import React, { useRef, useState } from 'react';
 import "./App.css"
 import { Canvas, useFrame, useThree } from '@react-three/fiber';
-import { Box, CameraControls, OrbitControls } from '@react-three/drei';
+import { Box, CameraControls } from '@react-three/drei';
 import Experience from './experience';
 import { Model } from './scene';
-import { VRButton, ARButton, XR, Controllers, Hands } from '@react-three/xr'
+import { VRButton, ARButton, XR, Controllers, Hands, useXR } from '@react-three/xr'
 import { TeleportationPlane } from '@react-three/xr'
-import {orbitControls} from "@react-three/drei"
+import { OrbitControls as OrbitControlsImpl } from 'three/examples/jsm/controls/OrbitControls';
+
+const OrbitControls = () => {
+  const { camera, gl } = useThree();
+  const orbitRef = React.useRef();
+
+  useFrame(() => {
+    if (orbitRef.current) orbitRef.current.update();
+  });
+
+  React.useEffect(() => {
+    orbitRef.current = new OrbitControlsImpl(camera, gl.domElement);
+    orbitRef.current.enableDamping = true; // Add damping for smooth rotation
+    orbitRef.current.dampingFactor = 0.1; // Adjust the damping factor as desired
+    orbitRef.current.target.set(0, 0, 0); // Set the orbit target (center of the scene)
+
+    return () => {
+      orbitRef.current.dispose();
+      orbitRef.current = null;
+    };
+  }, [camera, gl]);
+
+  return null;
+};
 
 function App() {
   return (
@@ -15,6 +38,7 @@ function App() {
       <Canvas>
       <XR>
         <Controllers />
+        <OrbitControls />
         <Hands />
         <TeleportationPlane
           /** Whether to allow teleportation from left controller. Default is `false` */
@@ -26,7 +50,7 @@ function App() {
           /** The radial size of the teleportation marker. Default is `0.25` */
           size={0.25}
         />
-        <OrbitControls />
+        
         <CameraControls speed={1.5} global zoom={0.7} />
         <Experience/>
       </XR>
