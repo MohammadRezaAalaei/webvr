5c49e8b60f354804ec7d0aada702310a49c09d19
Mohammadreza <Mohammadrezaaalaei@gmail.com>
2023-07-23 22:09:11 +0330

    orbit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React, { useRef, useState } from 'react';
 import "./App.css"
 import { Canvas, useFrame, useThree } from '@react-three/fiber';
-import { Box, CameraControls } from '@react-three/drei';
+import { Box, CameraControls, OrbitControls } from '@react-three/drei';
 import Experience from './experience';
 import { Model } from './scene';
 import { VRButton, ARButton, XR, Controllers, Hands } from '@react-three/xr'
 import { TeleportationPlane } from '@react-three/xr'
+import {orbitControls} from "@react-three/xr"
 
 function App() {
   return (
@@ -25,6 +26,7 @@ function App() {
           /** The radial size of the teleportation marker. Default is `0.25` */
           size={0.25}
         />
+        <OrbitControls/>
         <CameraControls speed={1.5} global zoom={0.7} />
         <Experience/>
       </XR>
