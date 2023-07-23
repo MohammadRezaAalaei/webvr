5f9ed6dbf0d3bec5e1f9f4f6f32fcf4508b71dfd
Mohammadreza <Mohammadrezaaalaei@gmail.com>
2023-07-23 21:34:54 +0330

    Adding Teleportation plane

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Box, CameraControls } from '@react-three/drei';
 import Experience from './experience';
 import { Model } from './scene';
 import { VRButton, ARButton, XR, Controllers, Hands } from '@react-three/xr'
+import { TeleportationPlane } from '@react-three/xr'
 
 function App() {
   return (
@@ -12,9 +13,18 @@ function App() {
       <VRButton />
       <Canvas>
       <XR>
-      
         <Controllers />
         <Hands />
+        <TeleportationPlane
+          /** Whether to allow teleportation from left controller. Default is `false` */
+          leftHand={true}
+          /** Whether to allow teleportation from right controller. Default is `false` */
+          rightHand={false}
+          /** The maximum distance from the camera to the teleportation point. Default is `10` */
+          maxDistance={10}
+          /** The radial size of the teleportation marker. Default is `0.25` */
+          size={0.25}
+        />
         <CameraControls speed={1.5} global zoom={0.7} />
         <Experience/>
       </XR>
